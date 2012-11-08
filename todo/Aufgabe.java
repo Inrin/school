32b5984c3f1c33ba556c3375058b2e73edf07bfc
seishinryohosha <riegeranton@zoho.com>
2012-11-08 20:27:58 +0100

    Added TodoList

---
+++ todo/Aufgabe.java
@@ -0,0 +1,16 @@
+public class Aufgabe
+{
+    private String beschreibung;
+    private Aufgabe next;
+
+    public Aufgabe(){
+    }
+
+    public String getBeschreibung(){
+        return beschreibung;
+    }
+
+    public Aufgabe getNext(){
+        return next;
+    }
+}
---
+++ todo/Warteschlange.java
@@ -0,0 +1,22 @@
+public class Warteschlange
+{
+	private Aufgabe first;
+
+	public Warteschlange(){}
+
+	public void addTask(){
+
+	}
+
+	public void deleteTask(){
+
+	}
+
+	public Aufgabe getFirst(){
+		return first;
+	}
+
+	public void printFirst(){
+		first.getBeschreibung();
+	}
+}
