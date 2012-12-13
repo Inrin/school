680183ddc7f31439d740f817bf3a0c83d1824d82
seishinryohosha <riegeranton@zoho.com>
2012-12-14 00:57:39 +0100

    Added basic construct of KeepOrThrow, with JUnits

--- KeepOrThrow/Card.java
+++ KeepOrThrow/Card.java
@@ -1,11 +1,66 @@
 import java.util.Random;
+/**
+ * Do what a card can do.
+ */
 public class Card
 {
     private int value;
     private Card next;
-
+    
+    /**
+     * Generate a card with random value.
+     */
     public Card(){
-        value = new Random().nextInt(13);
-        value += 2;
+        value = (new Random().nextInt(13)) +2;
+    }
+    
+    /**
+     * (debugging) Generate a card with specific value.
+     * 
+     * @param value of new created card.
+     */
+    public Card(int value){
+    }
+    
+    /**
+     * Return card beneath this on stack.
+     * 
+     * @return next as Card
+     */
+    public Card getNext(){
+        return null;
+    }
+    
+    /**
+     * Return value of this.
+     * 
+     * return value as int
+     */
+    public int getValue(){
+        return 0;
+    }
+    
+    /**
+     * Set next card
+     * 
+     * @param next card beneath this
+     */
+    public void setNext(Card next){
+        
+    }
+    
+    /**
+     * (debugging) Set value of this.
+     * 
+     * The value must be between 14-2(As normal playing cards).
+     * Else sets a value of 0.
+     * 
+     * @param value to be set as int.
+     */
+    public void setValue(int value){
+        if(value >1 && value <15)
+        this.value = value;
+        else
+        this.value = 0;
     }
 }
--- KeepOrThrow/Stack.java
+++ KeepOrThrow/Stack.java
@@ -1,7 +1,39 @@
-import java.util.Random;
+
+/**
+ * Stack of cards
+ */
 public class Stack
 {
-    private int value;
-    private Card top;
+   private Card top;
+   
+   /**
+    * Check emptiness.
+    * 
+    * @return true if empty
+    */
+   public boolean isEmpty(){
+       return false;
+    }
+   
+    /**
+     * Get the top card and put it back on this stack.
+     * 
+     * @return Topcard
+     */
+    public Card peek(){
+        return null;
+    }
     
+    /**
+     * Give the topcard, don't put it back.
+     */
+    public Card pop(){
+        return null;
+    }
+    
+    /**
+     * Put a Card on this Stack.
+     */
+    public void push(Card c){
+    }
 }
