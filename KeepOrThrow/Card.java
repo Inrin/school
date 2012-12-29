97e0d8f1b3125897991f1ba4e3854ba28148aa60
seishinryohosha <riegeranton@zoho.com>
2012-12-29 16:14:53 +0100

    Reduced Code with "(a>b) ? c:d" alike statements

--- KeepOrThrow/Card.java
+++ KeepOrThrow/Card.java
@@ -4,64 +4,61 @@ import java.util.Random;
  */
 public class Card
 {
-    private int value;
-    private Card next;
-    
-    /**
-     * Generate a card with random value.
-     */
-    public Card(){
-        value = (new Random().nextInt(13)) +2;
-    }
-    
-    /**
-     * (debugging) Generate a card with specific value.
-     * 
-     * @param value of new created card.
-     */
-    public Card(int value){
-        this.setValue(value);
-    }
-    
-    /**
-     * Return card beneath this on stack.
-     * 
-     * @return next as Card
-     */
-    public Card getNext(){
-        return next;
-    }
-    
-    /**
-     * Return value of this.
-     * 
-     * return value as int
-     */
-    public int getValue(){
-        return value;
-    }
-    
-    /**
-     * Set next card
-     * 
-     * @param next card beneath this
-     */
-    public void setNext(Card next){
-        this.next = next;
-    }
-    
-    /**
-     * (debugging) Set value of this.
-     * 
-     * The value must be between 14-2(As normal playing cards).
-     * Else sets a value of 0.
-     * 
-     * @param value to be set as int.
-     */
-    public void setValue(int value){
-        if(value >1 && value <15)
-        this.value = value;
-        else
-        this.value = 0;
-    }
+	private int value;
+	private Card next;
+
+	/**
+	 * Generate a card with random value.
+	 */
+	public Card(){
+		value = (new Random().nextInt(13)) +2;
+	}
+
+	/**
+	 * (debugging) Generate a card with specific value.
+	 * 
+	 * @param value of new created card.
+	 */
+	public Card(int value){
+		this.setValue(value);
+	}
+
+	/**
+	 * Return card beneath this on stack.
+	 * 
+	 * @return next as Card
+	 */
+	public Card getNext(){
+		return next;
+	}
+
+	/**
+	 * Return value of this.
+	 * 
+	 * return value as int
+	 */
+	public int getValue(){
+		return value;
+	}
+
+	/**
+	 * Set next card
+	 * 
+	 * @param next card beneath this
+	 */
+	public void setNext(Card next){///**///**
+		this.next = next;//*/
+	}
+
+	/**
+	 * (debugging) Set value of this.
+	 * 
+	 * The value must be between 14-2(As normal playing cards).
+	 * Else sets a value of 0.
+	 * 
+	 * @param value to be set as int.
+	 */
+	public void setValue(int value){///**
+		this.value = (value >1 && value <15) ? value:0; 	
+	}
 }
--- KeepOrThrow/Stack.java
+++ KeepOrThrow/Stack.java
@@ -4,60 +4,58 @@
  */
 public class Stack
 {
-    private Card top;
+	private Card top;
 
-    public Stack(){
-        this.top = null;
-    }
+	public Stack(){
+		this.top = null;
+	}
 
-    /**
-     * Check emptiness.
-     * 
-     * @return true if empty
-     */
-    public boolean isEmpty(){
-        if(top!=null)
-            return false;
-        else
-            return true;
-    }
+	/**
+	 * Check emptiness.
+	 * 
+	 * @return true if empty
+	 */
+	public boolean isEmpty(){
+		return (top!=null) ? false : true;
+	}
 
-    /**
-     * Get the top card and put it back on this stack.
-     * 
-     * @return Topcard
-     */
-    public Card peek(){
-        return top;
-    }
+	/**
+	 * Get the top card and put it back on this stack.
+	 * 
+	 * @return Topcard
+	 */
+	public Card peek(){
+		return top;
+	}
 
-    /**
-     * Give the topcard, don't put it back.
-     */
-    public Card pop(){
-        if(top!=null){
-            Card temp = top;
-            top = top.getNext();
-            temp.setNext(null);
-            return temp;
-        }
-        return null;
-    }
+	/**
+	 * Give the topcard, don't put it back.
+	 */
+	public Card pop(){
+		if(top!=null){///**
+			Card temp = top;
+			top = top.getNext();
+			temp.setNext(null);
+			return temp;
+		}else{
+			return null;
+		}//*/
+	}
 
-    /**
-     * Put a Card on this Stack.
-     */
-    public void push(Card c){
-        if(c!=null){
-            if(top == null){
-                top = c;
-            }else{
-                c.setNext(top);
-                top = c;
-            }
-        }else{
-            System.out.println("Must be a card!");
-        }
-    }
+	/**
+	 * Put a Card on this Stack.
+	 */
+	public void push(Card c){
+		if(c!=null){
+			if(top == null){
+				top = c;
+			}else{
+				c.setNext(top);
+				top = c;
+			}
+		}else{
+			System.out.println("Must be a card!");
+		}
+	}
 }
 
