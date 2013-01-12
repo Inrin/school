9c491c81618f230c14247652c37426943b3dfb21
seishinryohosha <riegeranton@zoho.com>
2013-01-12 15:26:44 +0100

    Added some new features.
    Added dynamic keepstackcounter.
    Added countdown to improve game experience.
    Added Menubar(Not finished).
    Added Reset-Game-Feature.
    Changed Count-Button to Reset-Button.
    Updated scorealgorithm.

--- KeepOrThrow/Game.java
+++ KeepOrThrow/Game.java
@@ -5,17 +5,22 @@ import java.awt.event.*;
 public class Game extends JFrame {
 
     private JButton jButton1,jButton2,jButton3;
-    private JLabel jLabel1,jLabel2,jLabel3;
+    private JLabel jLabels[] = new JLabel[5];
     private JPanel jPanel1,jPanel2;
-    private GameLogic game;
+    private JMenuBar jMenuBar;
+    private JMenu jMenuDatei;
+    private JMenuItem jMenuItemDateiHilfe,jMenuItemDateiSchließen;
+    private GameLogic gl;
     private String path;
+    private int countdown=60000;
+    private Timer timer;
 
     public Game() {
         initComponents();
     }
-    
+
     private void initComponents() {
-        game = new GameLogic();
+        gl = new GameLogic();
 
         switch(System.getProperty("os.name")){
             case "Windows 7":
@@ -24,19 +29,48 @@ public class Game extends JFrame {
             case "Linux":
             path="cards/";
             break;
-        
+
         }
 
         jPanel1 = new JPanel();
-        jLabel1 = new JLabel("Mixed Stack", new ImageIcon(path + "00.png"), JLabel.CENTER);
-        jLabel1.setVerticalTextPosition(JLabel.TOP);
-        jLabel1.setHorizontalTextPosition(JLabel.CENTER);
-        jLabel2 = new JLabel("Keep Stack", new ImageIcon(path + "00.png"), JLabel.CENTER);
-        jLabel2.setVerticalTextPosition(JLabel.TOP);
-        jLabel2.setHorizontalTextPosition(JLabel.CENTER);
-        jLabel3 = new JLabel("Throw Stack", new ImageIcon(path + "00.png"), JLabel.CENTER);
-        jLabel3.setVerticalTextPosition(JLabel.TOP);
-        jLabel3.setHorizontalTextPosition(JLabel.CENTER);
+        jMenuBar = new JMenuBar();
+        jMenuDatei = new JMenu("Datei");
+        jMenuItemDateiHilfe = new JMenuItem("Hilfe");
+        jMenuItemDateiHilfe.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent evt){
+                    jMenuItemDateiHilfeActionPerformed(evt);
+                }
+            });
+        jMenuItemDateiSchließen = new JMenuItem("Schließen");
+        jMenuItemDateiSchließen.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent evt){
+                    jMenuItemDateiSchließenActionPerformed(evt);
+                }
+            });
+            
+        jMenuDatei.add(jMenuItemDateiHilfe);
+        jMenuDatei.add(jMenuItemDateiSchließen);
+        jMenuBar.add(jMenuDatei);
+
+        jLabels[0] = new JLabel("Mixed Stack", new ImageIcon(path + "00.png"), JLabel.CENTER);
+        jLabels[1] = new JLabel("0", JLabel.CENTER);
+        jLabels[2] = new JLabel("Keep Stack", new ImageIcon(path + "00.png"), JLabel.CENTER);
+        jLabels[3] = new JLabel("120s", JLabel.CENTER);
+        jLabels[4] = new JLabel("Keep Stack", new ImageIcon(path + "00.png"), JLabel.CENTER);
+
+        Font f = new Font("Dialog",Font.PLAIN,18);
+        Font f2 = new Font("Dialog",Font.PLAIN,24);
+
+        for(int i=0; i<jLabels.length; i+=2){
+            jLabels[i].setVerticalTextPosition(JLabel.TOP);
+            jLabels[i].setHorizontalTextPosition(JLabel.CENTER);
+            jLabels[i].setFont(f);
+        }
+
+        for(int i=1; i<jLabels.length; i+=2){
+            jLabels[i].setFont(f2);
+        }
+
         jPanel2 = new JPanel();
         jButton1 = new JButton();
         jButton2 = new JButton();
@@ -45,12 +79,15 @@ public class Game extends JFrame {
         setTitle("Keep or throw!");
         setDefaultCloseOperation(Game.EXIT_ON_CLOSE);
         jPanel1.setLayout(new GridLayout(1, 0));
-        jPanel1.add(jLabel1);
-        jPanel1.add(jLabel2);
-        jPanel1.add(jLabel3);
-        updateLogic();
+
+        for(int i = 0; i<jLabels.length; i++)
+            jPanel1.add(jLabels[i]);
+
+        updateLabels();
         jPanel2.setLayout(new GridLayout(1, 0));
-        jButton1.setText("Keep");
+        jButton1.setFont(f);
+        jButton1.setText("Throw");
+        jButton1.setMnemonic(KeyEvent.VK_T);
         jButton1.addActionListener(new ActionListener(){
                 public void actionPerformed(ActionEvent evt){
                     jButton1ActionPerformed(evt);
@@ -58,7 +95,9 @@ public class Game extends JFrame {
             });
         jPanel2.add(jButton1);
 
-        jButton2.setText("Throw");
+        jButton2.setFont(f);
+        jButton2.setText("Keep");
+        jButton2.setMnemonic(KeyEvent.VK_K);
         jButton2.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent evt) {
                     jButton2ActionPerformed(evt);
@@ -66,14 +105,18 @@ public class Game extends JFrame {
             });
         jPanel2.add(jButton2);
 
-        jButton3.setText("Count");
+        jButton3.setFont(f);
+        jButton3.setText("Restart");
+        jButton3.setMnemonic(KeyEvent.VK_R);
         jButton3.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent evt) {
                     jButton3ActionPerformed(evt);
                 }
             });
         jPanel2.add(jButton3);
-
+        
+        setJMenuBar(jMenuBar);
+        
         GroupLayout layout = new GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -96,35 +139,78 @@ public class Game extends JFrame {
         );
 
         pack();
+        timerInit();
     }                        
 
+    private void jMenuItemDateiHilfeActionPerformed(ActionEvent evt){
+        JOptionPane.showMessageDialog(null, "See http://www.infoskript.com for more Info");
+    }
+
+    private void jMenuItemDateiSchließenActionPerformed(ActionEvent evt){
+        System.exit(0);
+    }
+
     private void jButton1ActionPerformed(ActionEvent evt) {     
-        game.keep();
-        updateLogic();
+        gl.dump();
+        updateLabels();
     }                                        
 
     private void jButton2ActionPerformed(ActionEvent evt) {                                         
-        game.dump();
-        updateLogic();
+        gl.keep();
+        updateLabels();
     }                                        
 
     private void jButton3ActionPerformed(ActionEvent evt) {
-        game.count();
+        countdown = 60000;
+        gl = new GameLogic();
+        updateLabels();
+        timer.restart();
     }
 
-    public void updateLogic(){
-        jLabel1.setIcon(new ImageIcon(path + 
-                                      game.view() + 
-                                      game.getMixedStack().peek().getSuite() + 
-                                      ".png"));
-        jLabel2.setIcon(new ImageIcon(path + 
-                                      game.getKeepStack().peek().getValue() + 
-                                      game.getKeepStack().peek().getSuite() +
-                                      ".png"));
-        jLabel3.setIcon(new ImageIcon(path + 
-                                      game.getThrowStack().peek().getValue() + 
-                                      game.getThrowStack().peek().getSuite() +
-                                      ".png"));
+    public void updateLabels(){
+        jLabels[0].setIcon(new ImageIcon(path + 
+                gl.view() + 
+                gl.viewSuite() + 
+                ".png"));
+        jLabels[2].setIcon(new ImageIcon(path + 
+                gl.getKeepStack().peek().getValue() + 
+                gl.getKeepStack().peek().getSuite() +
+                ".png"));
+        jLabels[4].setIcon(new ImageIcon(path + 
+                gl.getThrowStack().peek().getValue() + 
+                gl.getThrowStack().peek().getSuite() +
+                ".png"));
+    }
+
+    private void timerInit(){
+        final ActionListener playingtime = new ActionListener(){
+                public void actionPerformed(ActionEvent evt){
+                    jLabels[3].setText(String.valueOf(countdown/1000) + "s");
+                    countdown--;
+                }
+            };
+        ActionListener heartbeat = new ActionListener(){
+                public void actionPerformed(ActionEvent evt){
+                    jLabels[1].setText(String.valueOf(gl.count()));
+
+                    if(countdown == 0){
+                        timer.stop();
+                        while(!gl.getMixedStack().isEmpty()){
+                            gl.getMixedStack().pop();
+                            updateLabels();
+                        }
+                    }
+
+                    if(gl.getMixedStack().isEmpty()){
+                        timer.stop();
+                        JOptionPane.showMessageDialog(null, "Game over!\n You Score is: "
+                            + (gl.count() + scoreAlgorithm()));
+                    }
+                }
+            };
+        timer = new Timer(1,playingtime);
+        timer.addActionListener(heartbeat);
+        timer.start();
     }
 
     public static void main(String args[]) {
@@ -150,5 +236,23 @@ public class Game extends JFrame {
                     new Game().setVisible(true);
                 }
             });
+    }
+
+    private int scoreAlgorithm(){
+        int returnvalue = 0;
+        if(gl.getKeepStack().isEmpty())
+            return 0;
+
+        if(countdown > 50000)
+            returnvalue -= countdown/1000;
+        else 
+            returnvalue += countdown/1000;
+
+        if(gl.getThrowStack().isEmpty())
+            returnvalue += 1000;
+
+        returnvalue -= gl.getErrors();
+
+        return returnvalue;
     }
 }
