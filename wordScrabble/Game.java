b18aacb4dd43d14058428267d4fe27d88df78e50
seishinryohosha <riegeranton@zoho.com>
2013-01-15 16:02:35 +0100

    First implementation. Needs debuggins(I guess ;).

--- wordScrabble/Game.java
+++ wordScrabble/Game.java
@@ -1,42 +1,55 @@
 import java.util.LinkedList;
 public class Game{
-    private LinkedList<Karte> sentence;
-    private Karte currentCard;
-    private int drawnCards, deletedCards;
+	private LinkedList<Karte> sentence;
+	private Karte currentCard;
+	private int drawnCards, deletedCards;
 
-    public Game(){
-        LinkedList<Karte> sentence = new LinkedList<Karte>();
-    }
+	public Game(){
+	}
 
-    public void debugFillSentence(){
-        for(int i=0; i<10; i++){
-            sentence.add(new Karte());
-        }
-    }
+	public void debugFillSentence(){
+		for(int i=0; i<10; i++){
+			sentence.add(new Karte());
+		}
+	}
 
-    public void showCurrentCard(){
-        sentence.getLast();
-    }
+	public void showCurrentCard(){
+		return currentCard;
+	}
 
-    public void drawCard(){ 
-        sentence.add(new Karte());
-    }
+	public void drawCard(){ 
+		currentCard = new Karte();
+	}
 
-    public void putCard(int position){ 
-        
-    }
+	public void putCard(int position){ 
+		sentence.add(position);	
+	}
 
-    public void deleteCard(int position){
-    }
+	public void deleteCard(int position){
+		sentence.remove(position);
+	}
 
-    public void printInfo(){
-    }
+	public void printInfo(){
+		System.out.println("--------------------------------------------------------------------");
+		System.out.println("                          Word Scrabble");
+		System.out.println("--------------------------------------------------------------------");
+		System.out.println("Aktueller Satz: " +  printSentence();
+		System.out.println("Aktuelle Wortanzahl: " + satz.size());
+		System.out.println("Aktuelle Punktzahl: " + getScore());
+		System.out.println("Aktuelle Karte: " showCurrentCard + '\n');
+	}
 
-    public void printSentence(){
-    }
+	public void printSentence(){
+		for (Karte k : satz) {
+			System.out.print(k.getWort())
+		}
+	}
 
-    public int getScore(){
-        return 0;
-    }
+	public int getScore(){
+		int score = 0;
+		for (Karte k : satz) {
+			score += k.getWert();
+		}
+		return score;
+	}
 }
-
