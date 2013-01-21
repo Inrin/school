036b13f67ff473cad7fdb7738449388831eb35f1
seishinryohosha <riegeranton@zoho.com>
2013-01-21 03:17:17 +0100

    Added classes and their basic structure.

---
+++ tptb/Argument.java
@@ -0,0 +1,24 @@
+public class Argument{
+    private String argument;
+    private int relevance;
+
+    public Argument(){
+
+    }
+
+    private void setArgument(String argument){
+
+    }
+
+    private void setRelevance(int relevance){
+
+    }
+
+    public String getArgument(){
+        return argument;
+    }
+
+    public int getrelevance(){
+        return relevance;
+    }
+}
---
+++ tptb/TPTB.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+
+public class TPTB{
+    private LinkedList<Argument> pro;
+    private LinkedList<Argument> con;
+
+    public TPTB(){
+
+    }
+
+    public void printDecision(){
+
+    }
+
+    public void insertArgument(boolean con, String argument, int relevance){
+
+    }
+}
