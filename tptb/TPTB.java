901ed4cb8ddf99e23dd12bbda33b05822828876f
seishinryohosha <riegeranton@zoho.com>
2013-01-21 04:07:44 +0100

    Added first implementation - Javadoc.
    Will now start debugging.

--- tptb/TPTB.java
+++ tptb/TPTB.java
@@ -1,18 +1,71 @@
 import java.util.LinkedList;
 
 public class TPTB{
-    private LinkedList<Argument> pro;
-    private LinkedList<Argument> con;
-
+    /** The pro and con list */
+    private LinkedList<Argument> con, pro;
+    
+    /**
+     * Creates this with a pro- and a con-list.
+     */
     public TPTB(){
-
+        pro = new LinkedList<Argument>();
+        con = new LinkedList<Argument>();
     }
 
+    /**
+     * Prints the calculated decision.
+     */
     public void printDecision(){
-
+        System.out.println("========================================");
+        System.out.println("+            The decision is:          +");
+        System.out.println("========================================");
+        System.out.println("");
+        System.out.println("The pro-arguments are: \n");
+        for(Argument arg: pro){
+            System.out.println(arg.getArgument() + "(" + arg.getRelevance() + ")");
+        }
+        System.out.println("");
+        System.out.println("The con-arguments are: \n");
+        for(Argument arg: con){
+            System.out.println(arg.getArgument() + "(" + arg.getRelevance() + ")");
+        }
+        System.out.println("");
+        if(calculateDecision())
+            System.out.println("Do it!");
+        else
+            System.out.println("Don't do it");
     }
 
-    public void insertArgument(boolean con, String argument, int relevance){
+    /**
+     * Calculate decision.
+     * 
+     * @return true if it should be done
+     */
+    private boolean calculateDecision(){
+        int provalue = 0;
+        for(Argument arg: pro){
+            provalue += arg.getRelevance();
+        }
 
+        int convalue = 0;
+        for(Argument arg: con){
+            convalue += arg.getRelevance();
+        }
+
+        return (provalue > convalue) ? true : false;
+    }
+
+    /**
+     * Inserts a new argument to given list.
+     * 
+     * @param contra if true add to contra-list else to pro-list
+     * @param argument is the argument
+     * @param relevance is the importance of the argument
+     */
+    public void insertArgument(boolean contra, String argument, int relevance){
+        if(contra)
+            con.add(new Argument(argument,relevance));
+        else
+            pro.add(new Argument(argument,relevance));
     }
 }
