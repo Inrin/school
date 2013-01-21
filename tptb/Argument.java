8a0cd7eb5f5e00aabf578961feee877795900584
seishinryohosha <riegeranton@zoho.com>
2013-01-21 03:31:18 +0100

    Added basic implementation as mentioned in UML

--- tptb/Argument.java
+++ tptb/Argument.java
@@ -2,16 +2,33 @@ public class Argument{
     private String argument;
     private int relevance;
 
-    public Argument(){
-
+    public Argument(String argument, int relevance){
+        setArgument(argument);
+        setRelevance(relevance);
     }
 
+    /**
+     * Set argument of this.
+     * 
+     * @param argument as String must be not empty
+     */
     private void setArgument(String argument){
-
+        if(argument != null && !argument.isEmpty())
+            this.argument = argument;
+        else
+            System.err.println("null or \"\" is not permitted");
     }
 
+    /**
+     * Set relevance of this.
+     * 
+     * @param relevance as int, between 1(smallest) to 10(biggest) relevance
+     */
     private void setRelevance(int relevance){
-
+        if(relevance > 0 && relevance < 11)
+            this.relevance = relevance;
+        else
+            System.err.println("Relevance must be between 1-10");
     }
 
     public String getArgument(){
