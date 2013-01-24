27938bbeefff4cb54c84637d42779c4d484d19f8
seishinryohosha <riegeranton@zoho.com>
2013-01-24 22:11:36 +0100

    Fixed most errors. Can be used complete in bluej only

--- noRiskNoMoney/NoRiskNoMoney.java
+++ noRiskNoMoney/NoRiskNoMoney.java
@@ -2,16 +2,16 @@ import java.util.Random;
 
 public class NoRiskNoMoney{
     private boolean[] field = new boolean[6];
-    private boolean diceAllowed = true;
+    private boolean allowed= true;
 
     public NoRiskNoMoney(){
         for(boolean b: field)
             b = false;
     }
 
-    public void dice(boolean allowed){
+    public void dice(){
         if(allowed){
-            int r = new Random().nextInt(7);
+            int r = new Random().nextInt(6);
 
             if(!field[r])
                 field[r] = true;
@@ -22,15 +22,18 @@ public class NoRiskNoMoney{
     }
 
     public void printField(){
+        System.out.println("\f");
         System.out.println("=====================================================================");
         System.out.println("+                         No Risk No Money!");
         System.out.println("=====================================================================\n");
-        System.out.println("+---+---+---+");
-        for(int i=0; i<field.length; i+=3){
-            for(int j=0; j<2; i++){
-                System.out.println("| " + field[i] + " | " + field[i++] + " |\n");
-            }
+        System.out.println("+---+---+");
+        for(int i=0; i<5;){
+            int j=i;
+            System.out.println("| " + ((!field[j] ? ++j : "X") + " | " + ((!field[j]) ?  ++j: "X") + " |"));
+            i+=2;
         }
-        System.out.println("+---+---+---+");
+        System.out.println("+---+---+");
+        if(!allowed)
+            System.out.println("Game Over!\n Please restart...");
     }
 }
