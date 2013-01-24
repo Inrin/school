703b67d94a77ba79baadb4468623f419c0562beb
seishinryohosha <riegeranton@zoho.com>
2013-01-24 08:21:45 +0100

    Basic structure for new project noRiskNoMonkey

---
+++ noRiskNoMoney/NoRiskNoMoney.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class NoRiskNoMoney{
+    private boolean[] field = new boolean[6];
+    
+    public NoRiskNoMoney(){
+
+    }
+
+    public void dice(){
+
+    }
+    
+    public void printField(){
+        
+    }
+}
