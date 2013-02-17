f26f790684c5eda72106f4543ee7695d6b8cebcf
seishinryohosha <riegeranton@zoho.com>
2013-02-17 21:24:26 +0100

    Added some nice FileChooser - Fixed bug, that jLabel1 disapeared after values >=10

--- mp3player/MeineGUI.java
+++ mp3player/MeineGUI.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
 
 // Definiert eine neue Klasse als Unterklasse von JFrame
 public class MeineGUI extends JFrame implements MusicPlayerListener
@@ -8,79 +9,89 @@ public class MeineGUI extends JFrame implements MusicPlayerListener
     private JTextField jTextField1;
     private JButton jButton1,jButton2,jButton3;
     private JLabel jLabel1;
-    private Timer updateLabel;
-    
+    private JFileChooser fc;
+
     private MusicPlayer mplayer = new MusicPlayer();
-    
+
     public MeineGUI()
     {
         initComponents();
     }
-    
+
     private void initComponents(){
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         setTitle("Benzaiten");
-        
+
         jTextField1 = new JTextField();
         jButton1 = new JButton("Öffnen");
         jButton2 = new JButton("Play");
         jButton3 = new JButton("Stop");
         jLabel1 = new JLabel("0%");
-        
+        fc = new JFileChooser();
+
         jTextField1.setColumns(50);
-        
+        jTextField1.setEditable(false);
+
         jButton1.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent evt){
-                open(evt);
-            }
-        });
-        
+                public void actionPerformed(ActionEvent evt){
+                    open(evt);
+                }
+            });
+
         jButton2.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent evt){
-                play(evt);
-            }
-        });
-        
+                public void actionPerformed(ActionEvent evt){
+                    play(evt);
+                }
+            });
+
         jButton3.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent evt){
-                stop(evt);
-            }
-        });
-        
+                public void actionPerformed(ActionEvent evt){
+                    stop(evt);
+                }
+            });
+
         mplayer.addListener(this);
-        
-        setLayout(new FlowLayout());
-        
+
+        this.setLayout(new FlowLayout());
+
         this.add(jTextField1);
         this.add(jButton1);
         this.add(jButton2);
         this.add(jButton3);
         this.add(jLabel1);
-        
+
         pack();
         setVisible(true);
     }
-    
+
     public void positionChanged(int newPosition){
         jLabel1.setText(String.valueOf(newPosition) + "%");
+        pack();
     }
-    
+
     private void open(ActionEvent evt){
-        mplayer.open(jTextField1.getText());
+        int returnVal = fc.showOpenDialog(MeineGUI.this);
+
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            jLabel1.setText("0%");
+            File file = fc.getSelectedFile();
+            jTextField1.setText(file.getAbsolutePath());
+            this.setTitle("Benzaiten - " + file.getName());
+            mplayer.open(file.getAbsolutePath());
+        }
     }
-    
+
     private void play(ActionEvent evt){
         mplayer.play();
     }
-    
-    
+
     private void stop(ActionEvent evt){
         mplayer.stop();
     }
-    
+
     public static void main(String[] args){
         MeineGUI g = new MeineGUI();
     }
-    
+
 }
