0d53f2c685f9e835ad2092aa41f4aaa571cb2645
seishinryohosha <riegeranton@zoho.com>
2013-04-22 21:29:10 +0200

    HashMap

--- chatbot/src/Beantworter.java
+++ chatbot/src/Beantworter.java
@@ -1,8 +1,23 @@
+import java.util.HashMap;
+
 public class Beantworter {
+	private HashMap<String,String> antworten;
+
+	public Beantworter(){
+		antworten = new HashMap();
+	}
+
 	public String gibAntwort(String question){
-		if(!question.equals("exit"))
-		return " Aufgrund von Energiesparmaßnahmen wurde meine KI auf 1% "
-			+ "ihres Normalzustandes zurückversetzt.\n Daher verstehe ich \'"+question+"\' nicht.";
+		if(!question.toLowerCase().contains("exit") || !question.toLowerCase().contains("quit")){
+			String[] standardantwoorten = {" Aufgrund von Energiesparmaßnahmen wurde meine KI auf 1% "
+				+ "ihres Normalzustandes zurückversetzt.\n Daher verstehe ich \'"+question+"\' nicht.", " Bitte erzählen Sie mir mehr über: " +question};
+
+			if(antworten.containsKey(question))
+				System.out.println(antworten.get(question));
+					
+			return standardantwoorten[new java.util.Random().nextInt(2)];
+		}
+
 		return "";
 	}	
 }
