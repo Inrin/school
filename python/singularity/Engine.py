0e552aae76a60183ac10a266e02f75f6d54d4c49
seishinryohosha <riegeranton@zoho.com>
2013-06-20 17:55:34 +0200

    Basic GUI, no functionality

---
+++ python/singularity/Engine.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+
+def buttonBetClick():
+    """Bet to win some nice prices"""
+    pass
+
+def buttonPayClick():
+    """Yes I have won, give Player his money or take it all"""
+    pass
+
+def buttonDiceClick():
+    """Alea iacta est. Give the results"""
+    pass
--- python/singularity/Spiel.py
+++ python/singularity/Spiel.py
@@ -6,9 +6,16 @@
 
 from tkinter import *
 
+from Engine import *
+
 ##############################################################################
 ##                               Constants                                  ##
 ##############################################################################
+
+## Fentsergröße
+ROOTSIZE='700x200'
+
+## Spieltitel
 TITLE='Chuck-a-Luck'
 
 ## Segmenttitel
@@ -23,9 +30,10 @@ BUTTONDICE='Würfel werfen'
 
 ## Fonts
 FONT='Arial'
-FONTSIZET=18
+FONTSIZET=33
 FONTSIZEL=16
-FONTSIZWB=17
+FONTSIZEB=17
+FONTSIZEE=16
 
 ## Label Colors
 BGT='gray'
@@ -33,14 +41,26 @@ FGT='white'
 BGL='gray'
 FGL='white'
 
+## Input(Entry) Colors
+BGE='gray'
+FGE='white'
+
 ## Button Colors
 BGB='gray'
 FGB='white'
 
+## Frame Colors
+BGF='gray'
+
+## Layout padding
+PADX='5'
+PADY='5'
+
 ##############################################################################
 ##                                Fields                                    ##
 ##############################################################################
-RESULT=[0,0,0]
+CREDIT='0'
+RESULT=[0,1,2]
 
 ##############################################################################
 ##                                 GUI                                      ##
@@ -50,70 +70,91 @@ RESULT=[0,0,0]
 ##############################################################################
 ##                               Creation                                   ##
 ##############################################################################
-tkwindow = Tk()
-tkwindow.title(TITLE)
-tkwindow.geometry('500x500')
+root = Tk()
+root.title(TITLE)
+root.geometry(ROOTSIZE)
+
+##############################################################################
+##                                 Frames                                   ##
+##############################################################################
+
+frameResults = Frame(master=root, bg=BGF)
 
 ##############################################################################
 ##                                Labels                                    ##
 ##############################################################################
 
 ## Label für Titel
-labelTitle = Label(master=tkwindow, text=TITLE, fg=FGT, bg=BGT,
+labelTitle = Label(master=root, text=TITLE, fg=FGT, bg=BGT,
     font=(FONT, FONTSIZET))
 
 ## Label der Bereiche
-labelSegment1 = Label(master=tkwindow, text=SEGMENTT1, fg=FGL, bg=BGL, 
+labelSegment1 = Label(master=root, text=SEGMENTT1, fg=FGL, bg=BGL, 
     font=(FONT,FONTSIZEL))
-labelSegment2 = Label(master=tkwindow, text=SEGMENTT2, fg=FGL, bg=BGL, 
+labelSegment2 = Label(master=root, text=SEGMENTT2, fg=FGL, bg=BGL, 
     font=(FONT,FONTSIZEL))
-labelSegment3 = Label(master=tkwindow, text=SEGMENTT3, fg=FGL, bg=BGL, 
+labelSegment3 = Label(master=root, text=SEGMENTT3, fg=FGL, bg=BGL, 
     font=(FONT,FONTSIZEL))
 
 ## Label der Auswertungen
-labelCredit = Label(master=tkwindow, text=CREDIT, fg=FGL, bg=BGL, 
+labelCredit = Label(master=root, text=CREDIT, fg=FGL, bg=BGL, 
     font=(FONT,FONTSIZEL))
-labelResult1 = Label(master=tkwindow, text=RESULT[0], fg=FGL, bg=BGL, 
+labelResult1 = Label(master=frameResults, text=RESULT[0], fg=FGL, bg=BGL, 
     font=(FONT,FONTSIZEL))
-labelResult2 = Label(master=tkwindow, text=RESULT[1], fg=FGL, bg=BGL, 
+labelResult2 = Label(master=frameResults, text=RESULT[1], fg=FGL, bg=BGL, 
     font=(FONT,FONTSIZEL))
-labelResult3 = Label(master=tkwindow, text=RESULT[2], fg=FGL, bg=BGL, 
+labelResult3 = Label(master=frameResults, text=RESULT[2], fg=FGL, bg=BGL, 
     font=(FONT,FONTSIZEL))
 
 ##############################################################################
 ##                                Inputs                                    ##
 ##############################################################################
 
+entryBetInt = Entry(master=root, fg=FGE, bg=BGE, font=(FONT, FONTSIZEE))
+
 ##############################################################################
 ##                                Buttons                                   ##
 ##############################################################################
 
-buttonBet = Button(master=tkwindow, text=BUTTONBET, fg=FGB, bg=BGB, font=(FONT,
+buttonBet = Button(master=root, text=BUTTONBET, fg=FGB, bg=BGB, font=(FONT,
     FONTSIZEB), command=buttonBetClick)
-buttonBet = Button(master=tkwindow, text=BUTTONPAY, fg=FGB, bg=BGB, font=(FONT,
+buttonPay = Button(master=root, text=BUTTONPAY, fg=FGB, bg=BGB, font=(FONT,
     FONTSIZEB), command=buttonPayClick)
-buttonBet = Button(master=tkwindow, text=BUTTONDICE, fg=FGB, bg=BGB, font=(FONT,
+buttonDice = Button(master=root, text=BUTTONDICE, fg=FGB, bg=BGB, font=(FONT,
     FONTSIZEB), command=buttonDiceClick)
+
 
 ##############################################################################
 ##                                Layout                                    ##
 ##############################################################################
 
+## Putting in the grid
+labelTitle.grid(row=0, column=0, columnspan=3, padx=PADX, pady=PADY)
+labelSegment1.grid(row=1, column=0, padx=PADX, pady=PADY)
+labelSegment2.grid(row=1, column=1, padx=PADX, pady=PADY)
+labelSegment3.grid(row=1, column=2, padx=PADX, pady=PADY)
+labelCredit.grid(row=2, column=0, padx=PADX, pady=PADY)
+entryBetInt.grid(row=2, column=1, padx=PADX, pady=PADY)
+frameResults.grid(row=2, column=2, padx=PADX, pady=PADY)
+buttonBet.grid(row=3, column=0, padx=PADX, pady=PADY)
+buttonPay.grid(row=3, column=1, padx=PADX, pady=PADY)
+buttonDice.grid(row=3, column=2, padx=PADX, pady=PADY)
+
+## Putting in the frame
+labelResult3.pack(side='left', fill='both')
+labelResult2.pack(side='left', fill='both')
+labelResult1.pack(side='left', fill='both')
+
+
+## Resizeable, YES
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=1)
+root.columnconfigure(2, weight=1)
+root.columnconfigure(3, weight=1)
+root.rowconfigure(0, weight=1)
+root.rowconfigure(1, weight=1)
+root.rowconfigure(2, weight=1)
+root.rowconfigure(3, weight=1)
+
 ## aktiviere Fenster
-tkwindow.mainloop()
-
-##############################################################################
-##                                 Def's                                    ##
-##############################################################################
-
-def buttonBetClick():
-    """Bet to win some nice prices"""
-    pass
-
-def buttonPayClick():
-    """Yes I have won, give Player his money or take it all"""
-    pass
-
-def buttonDiceClick():
-    """Alea iacta est. Give the results"""
-    pass
+root.mainloop()
