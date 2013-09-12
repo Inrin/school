f86d0791859509bac43cd1740c469bcfaa71d3da
seishinryohosha <seishinryohosha@users.noreply.github.com>
2013-09-12 09:00:27 +0200

    Create pedesRobo.py
    First implementation

---
+++ python/miniworld/pedesRobo.py
@@ -0,0 +1,18 @@
+class PedesRobo(object):
+    """Lenkt die Lampen"""
+    state
+
+    def __init__(self, initstate):
+        self.state = initstate
+        
+    def getState():
+        return self.state
+        
+    def switch():
+        if self.state == 'rot':
+            state == 'gelb'
+        elif self.state == 'gelb':
+            state == 'gruen'
+        elif self.state == 'gruen'
+            state == 'rot'
+        
