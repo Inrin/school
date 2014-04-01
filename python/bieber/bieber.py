d0c50af686e6e3a6fa96f828acd0d0ab5c355a85
seishinryohosha <riegeranton@zoho.com>
2014-04-02 00:33:09 +0200

    .....

--- python/bieber/bieber.py
+++ python/bieber/bieber.py
@@ -5,16 +5,27 @@ def printDiagramm(n):
     friends = []
     sign = '<'
     for i in range(n):
-        friends.append(97+i) # CHar 97 = 'a'
+        friends.append(97+i) # Char 97 = 'a'
 
-    i=0
-    while(i < n-1):
-        print(chr(friends[i]), "   ",chr(friends[i+1]), "     ", end="")
-        i += 2
+    for i in range(0, n-1, 2):
+        print(chr(friends[i]), " ", chr(friends[i+1]), "    ", end="")
+
+    if n%2 != 0:
+        print(chr(friends[n-1]), end="")
+
+    print()
+
+
+    for i in range(0, n-1, 2):
+        print("-- --     ", end="")
+
+    if n%2 != 0:
+        print("-- --", end="")
+
     print()
 
 def whichFriendToVisit(n, d):
     if (d > n):
-        d %=n
+        d %= n
 
-printDiagramm(4)
+printDiagramm(7)
