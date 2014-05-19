fc5b023c54b175bacbdb175071e45e84d405a000
seishinryohosha <riegeranton@zoho.com>
2014-05-19 02:08:28 +0200

    They all still get assimilated...

--- python/rucksackproblem.py
+++ python/rucksackproblem.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from random import randint
+from random import shuffle
 
 gegenstaende = [(3.5, 375), (2.5, 300), (2.0, 100), (3.0, 225), (1.0, 50),
                (1.75, 125), (0.75, 75), (3.0, 275), (2.5, 150), (2.25, 50)]
@@ -36,7 +37,7 @@ def attribs(komb, weight=True, value=True):
     elif not weight and value:
         return wert
 
-def crossing(candidates):
+def crossing(candidates, mutationRate):
     splitter = randint(0, len(candidates[0]))
     child1 = candidates[0][0:splitter]
     child1 += candidates[1][splitter:len(candidates[1])]
@@ -44,15 +45,24 @@ def crossing(candidates):
     child2 = candidates[1][0:splitter]
     child2 += candidates[0][splitter:len(candidates[0])]
 
+    rand = randint(0, 100)
+
+    if rand <= mutationRate:
+        mutate(child1)
+        mutate(child2)
+
     return [child1, child2]
 
 def selection(population, maxWeight):
     quantity = len(population)-1
 
-    parents = [population[randint(0, quantity)] for x in range(4)]
+    parents = []
+    for x in range(4):
+        rand = randint(0, quantity)
+        parents.append(population[rand])
 
     for p in parents:
-        if attribs(p, weight=True, value=False) > maxWeight:
+        if attribs(p, weight=True, value=False) > maxWeight or 0:
             parents.remove(p)
 
     while len(parents) != 2:
@@ -67,14 +77,37 @@ def cycle(startPopulation, maxWeight, howLong):
     for i in range(howLong):
         newPopulation = []
         for j in range(4):
-            #newPopulation.append(crossing(selection(startPopulation, maxWeight)))
-            descendants = crossing(selection(startPopulation, maxWeight))
+            descendants = crossing(selection(startPopulation, maxWeight), 100)
             newPopulation += [c for c in descendants]
         startPopulation = newPopulation
         print(startPopulation)
     return startPopulation
 
-cycle(population, 15, 10)
+def mutate(individual):
+    rand = randint(0, len(individual)-1)
+    individualLocal = [int(x) for x in individual]
 
-def mutate():
-    pass
+    if individualLocal[rand] == 0:
+        individualLocal[rand] = 1
+    else:
+        individualLocal[rand] = 0
+
+    individual = [str(y) for y in individualLocal]
+
+
+def createBagpack(itemsCount):
+    backpack = ""
+    for i in range(itemsCount):
+        if randint(0,1) == 1:
+            backpack += '1'
+        else:
+            backpack += '0'
+    return backpack
+
+def createPopulation(howManyBacks, itemsCount):
+    population = []
+    for i in range(howManyBacks):
+        population.append(createBagpack(itemsCount))
+    return population
+
+print(cycle(createPopulation(1000, 8), 15, 100))
